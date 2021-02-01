8df04097c6b843472c0a072b34071db90176ff54
MalHibou <swperrin51@gmail.com>
2021-02-01 11:48:17 -0600

    set up signup form

--- anywhere-fitness/src/App.js
+++ anywhere-fitness/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login';
+import Signup from './components/Signup';
 import * as yup from 'yup';
 import schema from './validation/SignupSchema';
 
@@ -10,16 +11,39 @@ const initialLoginValues = {
   password: ''
 }
 
+const initialSignupValues = {
+  username: '',
+  password: '',
+  role: ''
+}
+
+const initialSignupErrors = {
+  username: '',
+  password: '',
+  role: ''
+}
+
 function App() {
   const [loginValues, setLoginValues] = useState(initialLoginValues);
+  const [signupValues, setSignupValues] = useState(initialSignupValues);
+  const [signupErrors, setSignupErrors] = useState(initialSignupErrors);
 
   const updateLogin = (name, value) => {
     setLoginValues({ ...loginValues, [name]:value });
   }
 
+  const updateSignup = (name, value) => {
+    setSignupValues( { ...signupValues, [name]:value });
+  }
+
   const submitLogin = () => {
     console.log("Here are the submitted values", {loginValues});
     setLoginValues(initialLoginValues);
+  }
+
+  const submitSignup = () => {
+    console.log("Here are the submitted values", {signupValues});
+    setSignupValues(initialSignupValues);
   }
 
   return (
@@ -29,6 +53,14 @@ function App() {
           values={loginValues}
           update={updateLogin}
           submit={submitLogin}
+        />
+      </Route>
+      <Route path='/signup'>
+        <Signup 
+          values={signupValues}
+          update={updateSignup}
+          submit={submitSignup}
+          errors={signupErrors}
         />
       </Route>
     </div>
